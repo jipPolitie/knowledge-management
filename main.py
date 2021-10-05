53d8cfd39134622c48b5fe1f97b98d8234d0baa8
Spark <-->
2021-10-05 18:45:20 +0200

    tmp

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import AIDA.Extract_Sentence_abstract
 import nltk
 nltk.data.path.append(r"C:\Users\lukac\PycharmProjects\knowledge-management\NLTK_data")
 
+from AIDA.Extract_Sentence_abstract import extract_sentences
 
+extract_sentences()
 
