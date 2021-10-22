7b54b2bf6d7ff9e5d040298ccfc782e9a7ca1d73
Spark <-->
2021-10-22 23:38:34 +0200

    made method from commented code

--- main.py
+++ main.py
@@ -13,23 +13,27 @@ stop = timeit.default_timer()
 print('Extraction Time: ', stop - start)
 
 
-# import utils
-#
-# pdf_folder = r'articles/10articels_about_wether_online_vocational_education/originals'
-# output_folder = r'articles/10articels_about_wether_online_vocational_education/'
-#
-# for file in os.listdir(pdf_folder):
-#     path = os.path.join(pdf_folder, file)
-#
-#     pdf = open(path, 'rb')
-#     text=utils.pdf2text(pdf).lower()
-#     beg_search_term=''
-#     end_search_term=''
-#     start=text.find(beg_search_term.lower())
-#     end=text.find(end_search_term.lower())
-#
-#     with open(os.path.join(output_folder, file[:-4]), 'w') as output_text_file:
-#         output_text_file.write(text[start:end])
+
+def half_manual_conversion():
+    global start
+    import utils
+    pdf_folder = r'articles/10articels_about_wether_online_vocational_education/originals'
+    output_folder = r'articles/10articels_about_wether_online_vocational_education/'
+    for file in os.listdir(pdf_folder):
+        path = os.path.join(pdf_folder, file)
+
+        pdf = open(path, 'rb')
+        text = utils.pdf2text(pdf).lower()
+        beg_search_term = ''
+        end_search_term = ''
+        start = text.find(beg_search_term.lower())
+        end = text.find(end_search_term.lower())
+
+        with open(os.path.join(output_folder, file[:-4]), 'w') as output_text_file:
+            output_text_file.write(text[start:end])
+
+
+
 
 
 
